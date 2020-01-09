240d2e257bf5bc0f14b7edcee06f01f7a5a23d8b
Assen Kolov <a.kolov@newmotion.com>
2020-01-09 17:32:27 +0100

    wip

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var server=http.createServer((function(request,response)
 	response.writeHead(200,
 	{"Content-Type" : "text/plain"});
 
+	const { method, url } = request
+	console.log("method = " + method)
+
   shell.exec('/Users/assenkolov/projects/gits-sync/runme.sh')
 	response.end("Hello World\n");
 }));
