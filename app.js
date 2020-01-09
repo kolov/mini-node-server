a112b4964049030b05a6d4fd824aac05dee6900c
Assen Kolov <a.kolov@newmotion.com>
2020-01-09 17:38:39 +0100

    wip

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var http=require('http')
+var http = require('http')
 const shell = require('shelljs')
-var server=http.createServer((function(request,response)
-{
-	response.writeHead(200,
-	{"Content-Type" : "text/plain"});
+var server = http.createServer((function (request, response) {
+	let body = [];
+	request.on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		body = Buffer.concat(body).toString();
 
-	const { method, url } = request
-	console.log("method = " + method)
+		response.writeHead(200,
+			{ "Content-Type": "text/plain" });
 
-  shell.exec('/Users/assenkolov/projects/gits-sync/runme.sh')
-	response.end("Hello World\n");
+		const { method, url } = request
+		console.log("method = " + method)
+		console.log("body = " + body)
+
+		shell.exec('/Users/assenkolov/projects/gits-sync/runme.sh')
+		response.end("processed\n");
+	});
 }));
 server.listen(7000);
